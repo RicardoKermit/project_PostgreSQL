2d3b222af24dd32f3abc40c0afabeb15b49c1147
RicardoKermit <8190313@estg.ipp.pt>
2024-11-09 01:49:16 +0000

    first endpoint test

--- src/main/java/org/example/controllers/BrandsController.java
+++ src/main/java/org/example/controllers/BrandsController.java
@@ -1,4 +1,29 @@
 package org.example.controllers;
 
+import jakarta.persistence.GeneratedValue;
+import org.example.models.Brand;
+import org.example.services.BrandsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/controller")
 public class BrandsController {
+
+    @Autowired
+    private BrandsService brandsService;
+
+    @GetMapping
+    ResponseEntity<List<Brand>> getAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(brandsService.getAll());
+    }
+
+    @PostMapping
+    ResponseEntity<Brand> add(@RequestBody Brand brand){
+        return ResponseEntity.status(HttpStatus.CREATED).body(brandsService.add(brand));
+    }
 }
